2b3ba5378d6953003aa7a97b3600e562e2b092f9
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2024-01-12 14:00:51 -0300

    cambio card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -9,11 +9,11 @@ import { cn } from '@/lib/utils'
 export default function Card({title,gradientTw, gradientCSS,encode}) {
   return (
     <article className="relative">
-        <Link href={`/${encode}`} >
+        <Link href={`/${encode}`}>
             <span className='sr-only'>Gradiente generado con HUE WAVE</span>
-          <div className={cn("size-64 rounded-xl", gradientTw)}></div>
+          <div className={cn("size-64 max-[320px]:size-56  rounded-xl", gradientTw)}></div>
         </Link>
-        <div className="w-full p-6 mx-auto -mt-6 text-white max-w-56 rounded-xl bg-neutral-900">
+        <div className="w-full p-6 mx-auto -mt-12 text-white max-w-56 rounded-xl bg-neutral-900/90">
           <p className="font-black tracking-wide uppercase">{title}</p>
           <div className="grid grid-cols-2 min-[320px]:grid-cols-4 items-center w-full gap-2 mt-4">
             <CopyTW gradient={gradientTw}/>
