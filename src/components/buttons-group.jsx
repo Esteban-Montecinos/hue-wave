a0ec8368e1239a379b33b1b1727dbb958dd9b7c7
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2024-01-16 16:57:29 -0300

    encode

--- src/components/buttons-group.jsx
+++ src/components/buttons-group.jsx
@@ -1,15 +1,31 @@
+"use client";
+
 import CopyTW from "./copy-tw";
 import CopyCSS from "./copy-css";
 import DownloadImage from "./download-image";
 import ShareGradient from "./share-gradient";
+import { encodeURI } from "js-base64";
+import { useStore } from "@/hooks/use-store";
 
 export default function ButtonsGroup({ handleClick }) {
+
+  const bg = useStore((state) => state.bg);
+  const fr = useStore((state) => state.fr);
+  const via = useStore((state) => state.via);
+  const to = useStore((state) => state.to);
+  const frPercent = useStore((state) => state.frPercent);
+  const viaPercent = useStore((state) => state.viaPercent);
+  const toPercent = useStore((state) => state.toPercent);
+  const encode = encodeURI(
+    `${bg.bg}+${bg.tw}+${fr.hex}+${fr.tw}+${via.hex}+${via.tw}+${to.hex}+${to.tw}+${frPercent.css}+${frPercent.tw}+${viaPercent.css}+${viaPercent.tw}+${toPercent.css}+${toPercent.tw}`
+  );
+  
   return (
     <div className="grid grid-cols-1 col-span-1 gap-4 lg:grid-cols-2">
       <CopyTW />
       <CopyCSS />
       <DownloadImage handleClick={handleClick} />
-      <ShareGradient />
+      <ShareGradient encode={encode} />
     </div>
   );
 }
--- src/components/share-gradient.jsx
+++ src/components/share-gradient.jsx
@@ -1,28 +1,13 @@
 "use client";
 
 import { Button } from "./ui/button";
-import { useStore } from "@/hooks/use-store";
 import { toast } from "sonner";
 import { IconShare } from "./icons/share-svg";
-import { encodeURI } from "js-base64";
 
 export default function ShareGradient({encode}) {
-  const bg = useStore((state) => state.bg);
-  const fr = useStore((state) => state.fr);
-  const via = useStore((state) => state.via);
-  const to = useStore((state) => state.to);
-  const frPercent = useStore((state) => state.frPercent);
-  const viaPercent = useStore((state) => state.viaPercent);
-  const toPercent = useStore((state) => state.toPercent);
 
   async function handleClick() {
-    let textEncode;
-    if (encode) {
-      textEncode = encode;
-    } else {
-      textEncode = encodeURI(`${bg.bg}+${bg.tw}+${fr.hex}+${fr.tw}+${via.hex}+${via.tw}+${to.hex}+${to.tw}+${frPercent.css}+${frPercent.tw}+${viaPercent.css}+${viaPercent.tw}+${toPercent.css}+${toPercent.tw}`)
-    }
-    await navigator.clipboard.writeText(`${location.origin}/${textEncode}`
+    await navigator.clipboard.writeText(`${location.origin}/${encode}`
     );
   }
   return (
