5cd4b945c763dbb07ea0cd5ab680edee260eee27
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2024-01-16 16:57:20 -0300

    link activo

--- src/components/active-link.jsx
+++ src/components/active-link.jsx
@@ -6,19 +6,23 @@ import { cn } from "@/lib/utils";
 
 export default function ActiveLink({ href, name }) {
   const pathname = usePathname();
-    let rute
+  let rute;
   if (pathname.slice("/")[1]) {
-    rute = "/generador"
-  }else{
-    rute = "/"
+    rute = "/generador";
+  } else {
+    rute = "/";
   }
   const isActive = rute === href;
-  
 
   return (
     <Link
       href={href}
-      className={cn(isActive ? "bg-neutral-700 text-white font-medium underline": "hover:bg-neutral-700 hover:text-white", "px-3 py-2 rounded transition-colors text-neutral-100")}
+      className={cn(
+        isActive
+          ? "bg-neutral-700 text-white font-medium"
+          : "hover:bg-neutral-700 hover:text-white bg-neutral-800",
+        "px-3 py-2 rounded transition-colors text-neutral-100"
+      )}
     >
       {name}
     </Link>
